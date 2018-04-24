f646864b7bdb32cabba41876a097390492e1d876
Louis Colin <l.colin@student.uclouvain.be>
2018-04-24 16:48:31 +0200

    test

---
+++ libfractal/fractal.h
@@ -0,0 +1,223 @@
+#ifndef _FRACTAL_H
+#define _FRACTAL_H
+
+struct fractal {
+  char *name;
+  int width;
+  int height;
+  double a;
+  double b;
+  double *value;// a voir si c'est pas plutot des int.
+};
+
+/*
+ * fractal_new: alloue une nouvelle structure fractal
+ *
+ * @name: nom de la fractale
+ * @width: largeur de l'image finale
+ * @height: hauteur de l'image finale
+ * @a: partie réelle des coordonnées de la fractale
+ * @b: partie imaginaire des coordonnées de la fractale
+ * @return: un pointeur vers une struct fractal, ou NULL si erreur
+ */
+struct fractal *fractal_new(const char *name, int width, int height, double a, double b){
+  struct fractal *new;
+  new=(struct fractal *) malloc(sizeof(struct fractal));
+  if(new==NULL){
+    return NULL;
+  }
+  new->name=name;
+  new->width=width;
+  new->height=height;
+  new->a=a;
+  new->b=b;
+  double value_test[height][width];
+  new->value=(double *)value_test;
+  return new;
+}
+
+/*
+ * fractal_free: libère la mémoire utilisée par une struct fractal
+ *
+ * @f: fractale à libérer
+ */
+void fractal_free(struct fractal *f){
+free(f);
+}
+
+/*
+ * fractal_get_name: retourne le nom de la fractale
+ *
+ * @f: fractale
+ * @return: nom de la fractale
+ */
+const char *fractal_get_name(const struct fractal *f);{
+  return (f->name);
+}
+
+/*
+ * fractal_get_value: retourne la valeur correspondant à un pixel de l'image
+ *
+ * @f: fractale
+ * @x: abscisse
+ * @y: ordonnée
+ * @return: valeur du pixel (x,y) de l'image de la fractale
+ */
+int fractal_get_value(const struct fractal *f, int x, int y){
+  int r=f->width;
+  return *(f->value+(y-1)*r+x);
+}
+
+/*
+ * fractal_set_value: défini la valeur correspondant à un pixel de l'image
+ *
+ * @f: fractale
+ * @x: abscisse
+ * @y: ordonnée
+ * @val: valeur
+ */
+void fractal_set_value(struct fractal *f, int x, int y, int val){
+  int r=f->width;
+  *(f->value+(y-1)*r+x)=val;
+}
+
+/*
+ * fractal_get_width: retourne la largeur de l'image de la fractale
+ *
+ * @f: fractale
+ * @return: largeur
+ */
+int fractal_get_width(const struct fractal *f){
+  return (f->width);
+}
+
+/*
+ * fractal_get_height: retourne la hauteur de l'image de la fractale
+ *
+ * @f: fractale
+ * @return: hauteur
+ */
+int fractal_get_height(const struct fractal *f){
+  return(f->height);
+}
+
+/*
+ * fractal_get_a: retourne la partie réelle des coordonnées de la fractale
+ *
+ * @f: fractale
+ * @return: partie réelle
+ */
+double fractal_get_a(const struct fractal *f){
+  return (f->a);
+}
+
+/*
+ * fractal_get_b: retourne la partie imaginaire des coordonnées de la fractale
+ *
+ * @f: fractale
+ * @return: partie imaginaire
+ */
+double fractal_get_b(const struct fractal *f){
+  return (f->b);
+}
+
+/*
+ * fractal_compute_value
+ *
+ * Applique la valeur de récurrence sur la fractale, aux coordonnées correspondantes
+ * au pixel (x,y) de l'image finale, et appelle fractal_set_value() pour
+ * enregistrer le nombre d'itérations effectuées.
+ *
+ * @f: fractale
+ * @x: abscisse
+ * @y: ordonnée
+ * @return: nombre d'itérations
+ */
+int fractal_compute_value(struct fractal *f, int x, int y){
+      double zx, zy;
+      double a, b;
+      int val;
+      int w, h;
+
+      a = fractal_get_a(f);
+      b = fractal_get_b(f);
+
+      w = fractal_get_width(f);
+      h = fractal_get_height(f);
+
+      zx = ((double)x / (double)w) * 3.0 - 1.5;
+      zy = ((double)y / (double)h) * 3.0 - 1.5;
+
+      val = iter_julia(zx, zy, a, b, 0);
+      fractal_set_value(f, x, y, val);
+
+      return val;
+}
+
+/*
+ * write_bitmap_sdl
+ *
+ * Transforme une fractale en un fichier bitmap (BMP)
+ *
+ * @f: fractale à transformer
+ * @fname: nom du fichier de sortie
+ * @return: 0 si pas d'erreurs, -1 sinon
+ */
+int write_bitmap_sdl(const struct fractal *f, const char *fname){
+  SDL_Surface *back;
+  SDL_Rect pix;
+  int w, h, i, j, col;
+  uint32_t pcol, rcol, gcol, bcol;
+  w = fractal_get_width(f);
+  h = fractal_get_height(f);
+
+  back = SDL_CreateRGBSurface(SDL_SWSURFACE, w, h, 32, RMASK, GMASK, BMASK, AMASK);
+  if (!back)
+      return -1;
+
+  for (i = 0; i < w; i++) {
+      for (j = 0; j < h; j++) {
+          col = itoc(fractal_get_value(f, i, j));
+          rcol = col & 0xff;
+          gcol = (col >> 8) & 0xff;
+          bcol = (col >> 16) & 0xff;
+          pcol = SDL_MapRGB(back->format, rcol, gcol, bcol);
+
+          pix.w = pix.h = 1;
+          pix.x = i;
+          pix.y = j;
+          SDL_FillRect(back, &pix, pcol);
+      }
+  }
+
+  if (SDL_SaveBMP(back, fname) < 0)
+      return -1;
+
+  SDL_FreeSurface(back);
+
+  return 0;
+}
+
+
+
+static int iter_julia(double zx, double zy, double a, double b, int it)
+{
+    /* prevent infinite loop */
+    if (it > MAX_ITER)
+        return 0;
+
+    /* prevent leaving Julia set
+     * if distance to origin >= 2
+     */
+    if (zx*zx + zy*zy >= 4.0)
+        return it;
+
+    /* compute next iteration
+     * f(z) = z^2 + c
+     * z = x + yi
+     * c = a + bi
+     */
+    return iter_julia(zx*zx - zy*zy + a, 2*zx*zy + b, a, b, it+1);
+}
+
+#endif
---
+++ libfractal/main.cpp
@@ -0,0 +1,252 @@
+#include <string.h>
+#include <stdint.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool>
+#include <unistd.h>
+#include <SDL.h>
+#include "fractal.h"
+#include <semaphore.h>
+#include <pthread.h>
+#define NTHREAD 3// a changer avec le nombre max de thread qu'on peut avoir!!!!!
+
+pthread_mutex_t mutex1;
+pthread_mutex_t mutex2;
+sem_t empty1;
+sem_t full1;
+sem_t empty2;
+sem_t full2;
+pthread_mutex_init (&mutex1, NULL);
+pthread_mutex_init (&mutex2,NULL);
+sem_init (&empty1,0,N);// N a discuter
+sem_init (&full1,0,0);
+sem_init (&empty2,0,N);//N a discuter
+sem_init (&full2,0,0);
+int le_plus_grand=INT_MIN;
+
+typedef struct tab{
+  int value;
+  struct fractal *tableau_fract;
+} tab_t;
+
+struct fractal test[6];//a initialise au meme nbre que &
+struct fractal test2[6];
+tab_t *tab1;//au départ, je les avais initialisé ds la main (probleme car ils étaient dans le stack->zone non accessible par les threads)
+tab1->value=0;//place dans le tableau a laquelle il faut placer la prochaine fractal dont il faut calculer la valeur des pixels.
+tab1->tableau_fract =(struct fractal *)test;
+tab_t *tab2;
+tab2->value=0;//place dans le tableau a laquelle il faut placer la prochaine fractal dont la valeur des pixels est deja calculee.
+tab2->tableau_fract=(struct fractal *)test2;
+
+
+
+
+
+
+//producteur
+void producer (void){
+  while(true){//a changer en while(le fichier a lire contient des trucs)
+    int r=open(filename,O_RDONLY);//attention, il faut surement mettre le filename en argument
+    if(r<0){
+      return -1;
+    }
+    char caracter;
+    int ret=read(r,&caracter,sizeof(char));
+    if(ret<0){
+      return (EXIT_FAILURE);
+    }
+    while(caracter==' '){
+      for(int w=0;caracter!='\n';w++){//pas certain du tt!! (\n est 2 caractère nn?)
+        ret=read(r,&caracter,sizeof(char));
+        if(ret<0){
+          return (EXIT_FAILURE);
+        }
+        if(ret==0){
+          //c'est la fin du fichier
+        }
+      }
+      ret=read(r,&caracter,sizeof(char));
+    }
+    while(caracter=='#'){
+      for(int w=0;caracter!='\n';w++){//pas certain du tt!! (\n est 2 caractère nn?)
+      ret=read(r,&caracter,sizeof(char));
+      if(ret<0){
+        return (EXIT_FAILURE);
+      }
+      if(ret==0){
+        //c'est la fin du fichier
+      }
+    }
+    ret=read(r,&caracter,sizeof(char));
+    }
+    char nom[65];
+    while(caracter!=' '){
+      strcpy(nom, caracter);
+      ret=read(r,&caracter,sizeof(char));
+      if(ret<0){
+        return (EXIT_FAILURE);
+      }
+    }
+    ret=read(r,&caracter,sizeof(char));// a faire car il faut passer au caractère suivant
+    if(ret<0){
+      return (EXIT_FAILURE);
+    }
+    char la_largeur[200];//a discuter j'ai mis 200 en ne sachant pas la longeure que pouvait avoir la longeur et la la_largeur mais on parle de 32 bits
+    while(caracter!=' '){
+      strcpy(la_largeur, caracter);
+      ret=read(r,&caracter,sizeof(char));
+      if(ret<0){
+        return (EXIT_FAILURE);
+      }
+    }
+    int la_largeur2=atoi(la_largeur);
+    ret=read(r,&caracter,sizeof(char));// a faire car il faut passer au caractère suivant
+    if(ret<0){
+      return (EXIT_FAILURE);
+    }
+    char la_longeur[200];
+    while(caracter!=' '){
+      strcpy(la_longeur, caracter);
+      ret=read(r,&caracter,sizeof(char));
+      if(ret<0){
+        return (EXIT_FAILURE);
+      }
+    }
+    int la_longeur2=atoi(la_longeur);
+    ret=read(r,&caracter,sizeof(char));
+    if(ret<0){
+      return (EXIT_FAILURE);
+    }
+    char reel[200];
+    while(caracter!=' '){
+      strcpy(reel,caracter);
+      ret=read(r,&caracter,sizeof(char));
+      if(ret<0){
+        return (EXIT_FAILURE);
+      }
+    }
+    float reel2=atof(reel);//transformer un char en float:atof
+    ret=read(r,&caracter,sizeof(char));
+    if(ret<0){
+      return (EXIT_FAILURE);
+    }
+    char imaginaire[200];
+    while(caracter!=' '){
+      strcpy(imaginaire,caracter);
+      ret=read(r,&caracter,sizeof(char));
+      if(ret<0){
+        return (EXIT_FAILURE);
+      }
+    }
+    float imaginaire2=atof(imaginaire);
+
+
+
+    //fin de la lecture d'une fractal.
+
+
+
+
+
+
+
+
+
+    struct fractal *fractal_a_creer;
+    fractal_a_creer=fractal_new(&nom,la_largeur2,la_longeur2,reel2,imaginaire2);
+    sem_wait(&empty1);
+    pthread_mutex_lock (&mutex1);
+    tab1->tableau_fract+tab1->value=fractal_a_creer;
+    tab1->value=tab1->value+1;
+    pthread_mutex_unlock(&mutex1);
+    sem_post(&full1);
+  }
+}
+
+
+void proconsumer(void){
+  struct fractal *fractal_a_calculer;
+  struct fractal *fractal_a_free;
+  while(true){//a changer en while(le producer a fini && le tableau est vide)
+    sem_wait(&full1);
+    pthread_mutex_lock(&mutex1);
+    fractal_a_free=tab1->tableau_fract+tab1->value-1;
+    fractal_a_calculer=fractal_new(fractal_a_free->name,fractal_a_free->width,fractal_a_free->height,fractal_a_free->a,fractal_a_free->b);
+    free(fractal_a_free);
+    tab1->value=tab1->value-1;  //prend une fractal + decremente la valeur du tableau et remplace l'endoit du tableau dont on a pri la fract par NULL
+    pthread_mutex_unlock(&mutex1);
+    sem_post (&empty1);
+    for(int largeur=0;largeur<*(fractal_a_calculer->height);largeur++){
+      for(int longueur=0;longeur<*(fractal_a_calculer->width);longeur++){
+        fractal_compute_value (fractal_a_calculer,longeur,largeur);//ATTENTION REVOIE UN INT
+      }
+    }//fin du calcul des fractal.
+    sem_wait(&empty2);
+    pthread_mutex_lock(&mutex2);
+    (tab2->tableau_fract+tab2->value)=fractal_a_calculer;
+    tab2->value=tab2->value+1;
+    pthread_mutex_unlock(&mutex2);
+    sem_post(&full2);
+
+  }
+}
+void consumer (void){
+  struct fractal *fractal_moyenne;
+  struct fractal *fractal_a_free;
+  double *b;
+  while(true){//a changer en while(le proconsummer a fini && le tableau est vide)
+    sem_wait(&full2);
+    pthread_mutex_lock(&mutex2);
+    fractal_a_free=tab2->tableau_fract+tab2->value-1;
+    fractal_moyenne=fractal_new(fractal_a_free->name,fractal_a_free->width,fractal_a_free->height,fractal_a_free->a,fractal_a_free->b);
+    b=fractal_a_free->value;//obligé de faire ca car fractal_new ne prend pas en compte le tableau value (ici, hyper important)
+    fractal_moyenne->value=b;
+    free(fractal_a_free);
+    tab2->value=tab2->value-1;//prend une fractal dont il faut calculer la moyenne +decremente et remplace l'endroit du tableau dont on a pris la fract par NULL
+    pthread_mutex_unlock(&mutex2);
+    sem_post(&empty2);
+    b=fractal_moyenne->value;
+    int max_tableau=fractal_moyenne->width * fractal_moyenne->height;
+    int a_comparer;
+    for(int i=0;i<max_tableau;i++){
+      a_comparer=a_comparer+*(b+i);
+    }
+    a_comparer=a_comparer/(max_tableau);//a cheker si on mettrait pas plutot un double et une division plus précise.
+    if(a_comparer>le_plus_grand){
+      le_plus_grand=a_comparer;
+    }
+
+  }
+
+}
+
+int main (int argc, char *argv[]){
+  int prod=NTHREAD/3;//a voir car pour moi il faudra peut etre mettre plus de thread 'proco' car ils ont bcp de travail donc seront plus lent.
+  pthread_t thread_pro[prod];//producteur qui lit les fractal et les met dans un tableau
+  pthread_t thread_proco[prod];//producteur et consommateur en mm tps (consomme les fractal et calcule leur valeur et les met dans deuxieme tableau)
+  pthread_t thread_co[prod];//consommateur final qui compare les valeur calculee
+  int err;
+  int i=0;
+  while (i<prod) {//permet de créer un producteur puis un consommateur et pas tt les prod puis tt les conso.
+    err=pthred_create(&(thread_pro[i]),NULL,producer,NULL);
+    if(err!=0){
+      error(err,"pthred_create");
+    }
+    err=pthred_create(&(thread_proco[i]),NULL,proconsumer,NULL);
+    if(err!=0){
+      error(err,"pthread_create");
+    }
+    err=pthread_create(&(thread_co[i]),NULL,consumer,NULL);
+    if(err!=0){
+      error(err,"pthread_create");
+    }
+
+    i++;
+  }
+
+
+}
